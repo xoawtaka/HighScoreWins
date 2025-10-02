66a9271b7177c9b8fcf8169840560f3b58149bac
Math Application <xoawtaka@gmail.com>
2025-10-02 11:36:49 -0400

    Completed Project

--- src/main/java/com/pluralsight/HighScoreParser.java
+++ src/main/java/com/pluralsight/HighScoreParser.java
@@ -8,37 +8,57 @@ public class HighScoreParser {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Please enter the game score.");
+        System.out.println("Please enter the game score.%n" +
+                "Utilize this format:%n" +
+                "(Team1:Team2|Team1Score:Team2Score)");
         String gameScore = input.nextLine();
 
         //game score for each team
-        String[] game = gameScore.split("|");
+        String[] game = gameScore.split("\\|");
 
+        //the teams and scores
+
+        /*
         String teams = game[0];
         String scores = game[1];
 
-
-        //split by ':'
-
+        //team split by ':'
         String[] team = teams.split(":");
-        String[] score = scores.split(":");
 
-        String winning = String.valueOf(team[0].equals(score[0]));
-        String winner = String.valueOf(team[1].equals(score[1]));
+        //score split by ':'
+        String[] score = scores.split(":");*/
 
-        System.out.println(winning + winner);
+        String[] team = game[0].split(":"); //home, away
+        String[] score = game[1].split(":"); //score1, score2
 
+        //differentiate team and score for corresponding
+        String homeTeam = team[0];
+        String awayTeam = team[1];
 
+        int homeScore = Integer.parseInt(score[0]);
+        int awayScore = Integer.parseInt(score[1]);
 
+        //combine correlating teams and scores
+        System.out.println("Home Team: " + homeTeam + " has " + homeScore + " points");
+        System.out.println("Away Team: " + awayTeam + " has " + awayScore + " points");
 
-        //differential team and score for corresponding
+        /*
+        String home = "Home: " + team[0] + ", Score: " + score[0];
+        String away = "Away: " + team[1] + ", Score: " + score[1];
 
+         */
 
-
-
-
-
-
+        if (homeScore > awayScore) {
+            System.out.println(homeTeam + " is the WINNER!!!");
+            System.out.println(awayTeam + " LOST... womp!!!");
+        }
+        else if (homeScore < awayScore) {
+            System.out.println(awayTeam + " is the WINNER!!!");
+            System.out.println(homeTeam + " LOST... womp!!!");
+        }
+        else {
+            System.out.println("OVERTIME!!!!");
+        }
 
 
     }
